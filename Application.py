c910adcccd560de2f46c4679377d2dcf969cf8dc
HI-HTT <tessa.derks@holland-innovative.nl>
2022-09-26 14:53:56 +0200

    Update Application.py

--- Application.py
+++ Application.py
@@ -143,5 +143,6 @@ def update_data(chosen_rows,piedropval,linedropval):
 
 #------------------------------------------------------------------
 
+
 if __name__ == '__main__':
     application.run(debug=True, host = '0.0.0.0', port = '80')
